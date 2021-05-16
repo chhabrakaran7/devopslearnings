17dd581155b144472a9f50b34b50ba71089ea9ef
chhabrakaran7 <chhabrakaran2020@gmail.com>
2021-05-16 15:44:38 +0100

    Add script to pull any docker image

---
+++ docker/pull_specific_dockerImage.sh
@@ -0,0 +1,24 @@
+## Pull docker image ##
+#
+# Copyright (c) Karan Chhabra 16/5/2021
+#
+# This script:
+# 1. Needs an argument supplied to it, This argument should be the docker image name one wants to pull
+# 2. Ex: sh -x pull_dockerImage.sh "jenkins/jenkins"
+#
+#
+
+#-- Pull docker image --#
+image=$1
+docker pull "$1"
+
+image_status=`docker images | grep "$image"`
+if [ $? -eq 0 ]; then
+  echo ""
+  image_id=`docker images | grep "$image" | awk -F' ' '{print $3}'`
+  echo "==================================================================="
+  echo "Docker image "$image" with IMAGE ID "$image_id" pulled successfully"
+  echo "==================================================================="
+else
+  echo "Docker failed to pull $image, Please check.."
+fi
