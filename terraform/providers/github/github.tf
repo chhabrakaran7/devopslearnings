796ecc9883143751e7c460d547dd495fd34a0727
chhabrakaran7 <chhabrakaran2020@gmail.com>
2021-11-14 20:46:52 +0000

    Changes to github repository terraform plan

--- terraform/providers/github/github.tf
+++ terraform/providers/github/github.tf
@@ -13,7 +13,7 @@ terraform {
 
 # Github provider authentication #
 provider "github" {
-  token = "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"
+  token = "<Please enter your github token here>>"
   owner = "chhabrakaran7"
 }
 
